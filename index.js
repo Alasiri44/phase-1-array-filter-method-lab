c1d16c31a654ff8054cbfc8ed0210ccd0f7de50c
alasiri <austinalasiri44@gmail.com>
2025-03-10 12:15:32 +0300

    Finished the 7 test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(myarray, name){
+    const finder = myarray.filter(element => element.toLowerCase() === name.toLowerCase());
+    return finder;
+}
+
+function fuzzyMatch(myarray, commonLetter){     
+    const finder = myarray.filter(element => element.startsWith(commonLetter));
+    return finder;
+}
+
+function matchName(myarray, matchingName){
+    const finder = myarray.filter(element => element.name === matchingName);
+    return finder;
+}
